90daa99c4e4e6bc8fe963cefe0e92c0c6610f365
Erik E. Zuñiga <ezuni3468@outlook.com>
2024-12-03 23:24:16 -0600

    ...

--- recetta/components/Recipes.tsx
+++ recetta/components/Recipes.tsx
@@ -1,40 +1,51 @@
-import {StyleSheet, Image, View, Text, Pressable} from 'react-native';
-import React from 'react';
-import MasonryList from '@react-native-seoul/masonry-list';
+import { StyleSheet, Image, View, Text, Pressable } from "react-native";
+import React from "react";
+import MasonryList from "@react-native-seoul/masonry-list";
 
-export default function Recipes({meals, categories}:{meals: any, categories: any}) {
+export default function Recipes({
+  meals,
+  categories,
+}: {
+  meals: any;
+  categories: any;
+}) {
   return (
     <View>
-      {
-        categories.length == 0 && meals.length == 0 ? null : (
-          <MasonryList
-            data={meals}
-            keyExtractor={(item) => item.idMeal}
-            numColumns={2}
-            showsVerticalScrollIndicator={true}
-            renderItem={({item, i}) => <RecipeCard item={item} index={i} />}
-            onEndReachedThreshold={0.1}
-          />
-        )
-      }
+      {categories.length == 0 && meals.length == 0 ? null : (
+        <MasonryList
+          data={meals}
+          keyExtractor={(item) => item.idMeal}
+          numColumns={2}
+          showsVerticalScrollIndicator={true}
+          renderItem={({ item, i }) => <RecipeCard item={item} index={i} />}
+          onEndReachedThreshold={0.1}
+        />
+      )}
     </View>
-  )
+  );
 }
 
-const RecipeCard = ({item, index}) => {
+const RecipeCard = ({ item, index }: { item: any; index: any }) => {
   return (
     <View>
-      <Pressable style={{width: '100%', flex: 1, justifyContent: 'center'}}>
+      <Pressable style={{ width: "100%", flex: 1, justifyContent: "center" }}>
         <Image
-          source={{uri: item.strMealThumb}}
-          style={{width: '100%', height: 200, borderRadius: 15}}
+          source={{ uri: item.strMealThumb }}
+          style={{ width: "100%", height: 200, borderRadius: 15 }}
         />
-        <Text style={{fontSize: 17.5, fontWeight: '500', marginHorizontal: 15, marginTop: 10}}>
-          {
-            item.strMeal.length >= 20 ? item.strMeal.slice(0, 20) + '...' : item.strMeal
-          }
+        <Text
+          style={{
+            fontSize: 17.5,
+            fontWeight: "500",
+            marginHorizontal: 15,
+            marginTop: 10,
+          }}
+        >
+          {item.strMeal.length >= 20
+            ? item.strMeal.slice(0, 20) + "..."
+            : item.strMeal}
         </Text>
       </Pressable>
     </View>
-  )
-}
+  );
+};
